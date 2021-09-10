65854515e8ddcf752eca7c4c55287b74728f15f3
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-10 05:12:35 +0000

    finish 4-1

--- chapter4/config/webpack/environment.js
+++ chapter4/config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 
+environment.loaders.prepend('erb', erb)
+environment.plugins.prepend('provide', provide)
 module.exports = environment
