2344adda127eec3b7ab065ecb0371bba6b0876b7
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-26 02:44:46 +0000

    イベント作成機能を作成した

--- awesome_events/config/application.rb
+++ awesome_events/config/application.rb
@@ -28,8 +28,8 @@ module AwesomeEvents
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
